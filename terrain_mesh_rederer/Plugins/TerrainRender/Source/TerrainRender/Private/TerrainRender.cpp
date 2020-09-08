574869eac3ca611ab3c13ec5d4ceaa8c0dba6e79
Prem Kumar Chedella <prem.chedella@gmail.com>
2020-09-08 09:25:42 +0300

    UE:Mesh Renderer:Editor Module: added Data and Parser functions.

--- terrain_mesh_rederer/Plugins/TerrainRender/Source/TerrainRender/Private/TerrainRender.cpp
+++ terrain_mesh_rederer/Plugins/TerrainRender/Source/TerrainRender/Private/TerrainRender.cpp
@@ -5,6 +5,13 @@
 #include "TerrainRenderEditor.h"
 #include "LevelEditor.h"
 
+#include "DemElevationParser.h"
+#include "DemElevationData.h"
+
+// File Open Diaglog includes
+#include "Developer/DesktopPlatform/Public/IDesktopPlatform.h"
+#include "Developer/DesktopPlatform/Public/DesktopPlatformModule.h"
+
 #define LOCTEXT_NAMESPACE "FTerrainRenderModule"
 
 void FTerrainRenderModule::StartupModule()
@@ -56,6 +63,80 @@ void FTerrainRenderModule::AddToolbarButton(FToolBarBuilder& Builder)
 void FTerrainRenderModule::TerrainRenderAction()
 {
   UE_LOG(LogTemp, Warning, TEXT("Terrain Render Button Pressed.!!!"));
+
+  FString FileName = GetFileName();
+  FileParse(FileName);
+
+  PrintData();
+
+  
+}
+
+FString FTerrainRenderModule::GetFileName()
+{
+  FString FileName = "";
+
+  IDesktopPlatform* DesktopPlatform = FDesktopPlatformModule::Get();
+  if (DesktopPlatform)
+  {
+    //A value of 0 represents single file selection while a value of 1 represents multiple file selection
+    uint32 SelectionFlag = 0;
+    FString DialogTitle = FString("DEM Elevation");
+    FString DefaultPath = FString(".");
+    FString FileTypes = FString("Text Files | *.asc");
+    TArray<FString> OutFileNames;
+    DesktopPlatform->OpenFileDialog(NULL,
+      DialogTitle, DefaultPath,
+      FString(""),
+      FileTypes,
+      SelectionFlag,
+      OutFileNames);
+
+    if (OutFileNames.Num() > 0)
+    {
+      UE_LOG(LogTemp, Warning, TEXT("File Dialog: Opened File = %s"),
+          *OutFileNames[0]);
+      FileName = OutFileNames[0];
+    }
+  }
+
+  return FileName;
+}
+
+void FTerrainRenderModule::FileParse(const FString& FileName)
+{
+  DemElevationParser parser(FileName);
+  parser.Parse();
+}
+
+void FTerrainRenderModule::PrintData()
+{
+  DemElevationData* DemElevation = DemElevationData::Instance();
+
+  int32 NoCols = DemElevation->GetNumCols();
+  UE_LOG(LogTemp, Warning, TEXT("Mesh: No. Cols = %d"), NoCols);
+
+  int32 NoRows = DemElevation->GetNumRows();
+  UE_LOG(LogTemp, Warning, TEXT("Mesh: No. Rows = %d"), NoRows);
+
+  int32 XCorner = DemElevation->GetXCorner();
+  UE_LOG(LogTemp, Warning, TEXT("Mesh: X Lower Level Corner = %d"), XCorner);
+
+  int32 YCorner = DemElevation->GetXCorner();
+  UE_LOG(LogTemp, Warning, TEXT("Mesh: Y Lower Level Corner = %d"), YCorner);
+
+  int32 NoData = DemElevation->GetNoData();
+  UE_LOG(LogTemp, Warning, TEXT("Mesh: No Data Value = %d"), NoData);
+
+  float CellSize = DemElevation->GetCellSize();
+  UE_LOG(LogTemp, Warning, TEXT("Mesh: Cell Size = %f"), CellSize);
+
+  TArray<TArray<int32>> ElevationData = DemElevation->GetData();
+
+  UE_LOG(LogTemp, Warning, TEXT("Mesh: Number of Colums = %d "),
+      ElevationData.Num());
+  UE_LOG(LogTemp, Warning, TEXT("Mesh: Values in Each Row = %d "),
+      ElevationData[0].Num());
 }
 
 #undef LOCTEXT_NAMESPACE
